5e1dcfc259a50c41f7b76d869edf71186d7db159
Adria Blancafort <adriablancafort@gmail.com>
2024-09-24 14:02:05 +0200

    add random user agents

--- product_scraper/settings.py
+++ product_scraper/settings.py
@@ -50,9 +50,10 @@ ROBOTSTXT_OBEY = False
 
 # Enable or disable downloader middlewares
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
-# DOWNLOADER_MIDDLEWARES = {
-#    "product_scraper.middlewares.ProductScraperDownloaderMiddleware": 543,
-# }
+DOWNLOADER_MIDDLEWARES = {
+    "scrapy.downloadermiddlewares.useragent.UserAgentMiddleware": None,
+    "scrapy_user_agents.middlewares.RandomUserAgentMiddleware": 400,
+}
 
 # Enable or disable extensions
 # See https://docs.scrapy.org/en/latest/topics/extensions.html
